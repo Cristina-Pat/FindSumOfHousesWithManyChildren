66a182a496262034a9b54ff77bd1ab5c180872a8
Cristina Pat <calota.cristina@gmail.com>
2022-06-01 10:35:49 +0100

    first commit

---
+++ src/findSumOfHousesWithManyChildren/Main.java
@@ -0,0 +1,41 @@
+package findSumOfHousesWithManyChildren;
+
+import java.util.HashSet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Test t = new Test();
+	t.children.put(1, new HashSet<String>());
+	t.children.put(2, new HashSet<String>());
+	t.children.put(3, new HashSet<String>());
+	t.children.put(4, new HashSet<String>());
+
+	t.children.get(1).add("Joe");
+	t.children.get(1).add("Bill");
+	t.children.get(1).add("Frank");
+	t.children.get(1).add("Arthur");
+	t.children.get(2).add("Havel");
+	t.children.get(2).add("Elisabet");
+	t.children.get(2).add("Josef");
+	t.children.get(3).add("Mustafa");
+
+	t.moreThan3Children();
+	
+	Test t2 = new Test();
+	t2.children.put(1, new HashSet<String>());
+	t2.children.put(2, new HashSet<String>());
+	t2.children.put(3, new HashSet<String>());
+	t2.children.put(4, new HashSet<String>());
+
+	t2.children.get(1).add("Frank");
+	t2.children.get(1).add("Arthur");
+	t2.children.get(2).add("Havel");
+	t2.children.get(2).add("Elisabet");
+	t2.children.get(3).add("Mustafa");
+
+	t2.moreThan3Children2();
+	
+	}
+}
---
+++ src/findSumOfHousesWithManyChildren/Test.java
@@ -0,0 +1,54 @@
+package findSumOfHousesWithManyChildren;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Test 
+{
+    HashMap<Integer, HashSet<String>> children;
+    
+    //constructor
+    public Test()
+    {
+        children = new HashMap<Integer, HashSet<String>>();
+    }
+    
+    public void moreThan3Children(){
+        
+        HashSet<Integer> housesWithMoreThan3 = new HashSet<Integer>();
+        
+        int houses = 0;
+        
+        for(Integer aHouse : children.keySet()) {
+            if (children.get(aHouse).size() >= 3) {
+                housesWithMoreThan3.add(aHouse);
+            }
+        }
+        houses = housesWithMoreThan3.size();
+        
+        if(houses > 0) {
+            System.out.println(houses + " houses have three or more children");
+        } else {
+            System.out.println("No houses have three or more children");
+        }
+    }
+    
+    
+     public void moreThan3Children2(){
+        
+        int countHouses = 0;
+        
+        for(Integer aHouse : children.keySet()) {
+            if (children.get(aHouse).size() >= 3) {
+                countHouses++;
+            }
+        }
+        
+        if(countHouses > 0) {
+            System.out.println(countHouses + " houses have three or more children");
+        } else {
+            System.out.println("No houses have three or more children");
+        }
+    }
+       
+}
